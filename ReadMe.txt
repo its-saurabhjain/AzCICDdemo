71bf28b89f4709fed5de315bcbcac96c39b0586d
Saurabh Jain <Jain-Saurabh@hcl.com>
2019-07-03 13:30:55 -0700

    test build

--- ReadMe.txt
+++ ReadMe.txt
@@ -1,1 +1,3 @@
 https://medium.com/@tomershaiman/building-a-ci-cd-pipeline-on-azure-kubernetes-service-aks-part-2-1f43a922088c
+
+---Test Build
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const config = require('config')
 const app = express()
 const port =  config.server.port || 8080
-
+//const port = process.env.PORT || 8080
 console.log('app data is ' + config.app["my-data"])
 
 app.get('/', (req, res) => res.send('Hello World!'))
